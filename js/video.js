e1683d644b4d545645c5eef4a81c8972e3945dce
AdamKim <4_2@naver.com>
2023-07-28 17:08:00 +0900

    Add day2

--- js/video.js
+++ js/video.js
@@ -14,16 +14,38 @@ function createVideoItem(video_id) {
 
       // 데이터 있는지 확인
       if (response && response.video_id !== undefined) {
-        // 각 데이터들을 콘솔에 출력
-        console.log(response.video_id);
-        console.log(response.image_link);
-        console.log(response.upload_date);
-        console.log(response.video_channel);
-        console.log(response.video_detail);
-        console.log(response.video_link);
-        console.log(response.video_tag);
-        console.log(response.video_title);
-        console.log(response.views);
+        let image_link = response.image_link;
+        let upload_date = response.upload_date;
+        let video_channel = response.video_channel;
+        let video_detail = response.video_detail;
+        let video_id = response.video_id;
+        let video_link = response.video_link;
+        let video_tag = response.video_tag;
+        let video_title = response.video_title;
+        let views = response.views;
+
+        // html에 요소 넣기
+        let videoContainer = document.createElement("div");
+
+        let videoTitle = document.createElement("h2");
+        videoTitle.textContent = video_title;
+        videoContainer.appendChild(videoTitle);
+
+        let videoImage = document.createElement("img");
+        videoImage.src = image_link;
+        videoContainer.appendChild(videoImage); //
+
+        let videoChannel = document.createElement("p");
+        videoChannel.textContent = video_channel;
+        videoContainer.appendChild(videoChannel);
+
+        let videoViews = document.createElement("p");
+        videoViews.textContent = "조회수" + views + "회";
+        videoContainer.appendChild(videoViews);
+
+        // FEED에 넣기
+        let feed = document.getElementById("feed");
+        feed.appendChild(videoContainer);
 
         // 다음 video_id로 재귀 호출
         createVideoItem(video_id + 1);
